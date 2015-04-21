1a65d3a6d91082c256aedbde69f30327e0d29cd6
Emmanuel velez <eava745@gmail.com>
2015-04-21 15:53:33 -0500

    Rest con edit

--- app/controllers/api/v1/usuarios_controller.rb
+++ app/controllers/api/v1/usuarios_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::UsuariosController < Api::V1::BaseController
+skip_before_action :require_authentication!
 
 def update#Put verbo http
-	
-	 current_usuario.update();
+	usuario = Usuario.find(params[:id])
+	usuario.update(total_matados:params[:matados],muertes: params[:muertes])
+	 #current_usuario.update();
 	
 end
 
