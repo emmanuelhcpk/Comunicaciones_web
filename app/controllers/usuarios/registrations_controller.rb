b451303c8d9b06094269536716ade85bf33eff95
Emmanuel velez <eava745@gmail.com>
2015-04-21 22:36:48 -0500

    Update y perfil arregados

--- app/controllers/usuarios/registrations_controller.rb
+++ app/controllers/usuarios/registrations_controller.rb
@@ -46,7 +46,7 @@ class Usuarios::RegistrationsController < Devise::RegistrationsController
   # You can put the params you want to permit in the empty array.
    def configure_account_update_params
     devise_parameter_sanitizer.for(:account_update) << :nickname
-  # end
+   end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
